ecf92631ebf958a64a46aabca20e4f469283fc16
Aleksandar <Sasho80@gbg.bg>
2023-04-16 18:11:30 +0300

    Add files via upload

---
+++ ExcellentOrNot.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+	double grade;
+	cin >> grade;
+	if (grade >=5.50) {
+		cout << "Excellent!" << endl;
+	}
+	else {
+		cout << "Not excellent." << endl;
+	}
+	return 0;
+}
